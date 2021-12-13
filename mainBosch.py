37eb1658e84749ec955839e080901a0549fda425
MBoscho <martiboschpla@gmail.com>
2021-12-13 18:07:56 +0100

    19.07

--- mainBosch.py
+++ mainBosch.py
@@ -1,3 +1,15 @@
+# from selenium import webdriver
+
+# driver.get('https://www.stw-thueringen.de/mensen/ilmenau/mensa-ehrenberg.html')
+
+# find_searchbox = driver.findElement(By.id(“text-box”));
+
 from selenium import webdriver
-driver = webdriver.Firefox(executable_path=r'Driver\geckodriver.exe')
+from selenium.webdriver.firefox.options import Options, By
+
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(executable_path=r'Driver\geckodriver.exe', options=options)
 driver.get('https://www.stw-thueringen.de/mensen/ilmenau/mensa-ehrenberg.html')
+
+# element=driver.find_element(By.ID,"speiseplan")
